e529d3bb880064abccd781f29f6108e25377fd96
nefelip <npappa@live.com>
2018-11-07 22:20:28 +0200

    more additions

--- app/src/main/java/com/exaple/android/quidditchscores/MainActivity.java
+++ app/src/main/java/com/exaple/android/quidditchscores/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     int SlytherinPoints = 0;
     int GriffindorPoints = 0;
+    /*  playGame: as long this is true the game continues
+    *   when the player scores the snitch the game ends
+    * */
     boolean playGame=true;
 
     @Override
@@ -16,16 +19,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /* Display the Slytherin's score */
     public void displayForSlytherin() {
         TextView scoreView = (TextView) findViewById(R.id.slytherinScore);
         scoreView.setText(String.valueOf(SlytherinPoints));
     }
 
+    /* Display the Griffindor's score */
     public void displayForGriffindor() {
         TextView scoreView = (TextView) findViewById(R.id.griffindorScore);
         scoreView.setText(String.valueOf(GriffindorPoints));
     }
-
+    /* Display the result string if there is a winner or if it's a draw */
     public void declaireWinner() {
         String winner = "";
         if(SlytherinPoints == 0 &&  GriffindorPoints==0){
@@ -59,14 +64,15 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
+    /* Grifindor scores 10 points if the game is still on going */
     public void griffindorScoresQuaffle(View v) {
         if(playGame) {
             GriffindorPoints = GriffindorPoints + 10;
             displayForGriffindor();
         }
     }
-
+    /* Grifindor scores 150 points if the game is still on going
+    *   the game ends and we declare the winner*/
     public void griffindorScoresSnitch(View v) {
         if (playGame) {
             GriffindorPoints = GriffindorPoints + 150;
@@ -75,14 +81,15 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+    /* Slytherin scores 10 points if the game is still on going */
     public void slytherinScoresQuaffle(View v) {
         if (playGame) {
             SlytherinPoints = SlytherinPoints + 10;
             displayForSlytherin();
         }
     }
-
+    /* Slytherin scores 150 points if the game is still on going
+     *   the game ends and we declare the winner*/
     public void SlytherinScoresSnitch(View v) {
         if (playGame) {
             SlytherinPoints = SlytherinPoints + 150;
@@ -90,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
             declaireWinner();
         }
     }
+    /* Game reset and starts over*/
     public void resetGame(View v){
         playGame=true;
         SlytherinPoints=0;
